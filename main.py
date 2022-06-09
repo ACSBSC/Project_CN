13baf7933c51f7a44bad1b3299fcbbba0abb6d1c
ACSBSC <36998939+ACSBSC@users.noreply.github.com>
2022-06-09 12:28:06 +0200

    iniciar network sin población

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 #################### Imports ####################
 import numpy as np
 import os
+import networkx as nx
+import pandas as pd
+import matplotlib.pyplot as plt
 
 #################### Settings ####################
 CSV_FILEPATH = os.path.join("FW_005", "FW_005.csv")
+CSV_FILEPATH_2 = os.path.join("FW_005", "FW_005-species.csv")
 SELF_LIMITATION = 0.1
 MUTUALISM = 0.1
 COMPETITION = 0.001
 
+REPRODUCTION_RATE_PRAY = 1.5
+REPRODUCTION_RATE_PREDATOR = 1
 
 #################### Functions ####################
 def create_iteraction_matrix(filepath=CSV_FILEPATH, self_limitation=SELF_LIMITATION,
@@ -39,6 +45,38 @@ def create_iteraction_matrix(filepath=CSV_FILEPATH, self_limitation=SELF_LIMITAT
     return inter_matrix
 
 
+def create_network(inter_matrix, filepath=CSV_FILEPATH_2):
+    df = pd.read_csv(filepath)  
+    species=[]
+    for specie in df["Specie"]:
+        if specie not in species:
+            species.append(specie)
+    
+    G = nx.Graph()
+    
+    for new_node in range(len(species)):
+        G.add_nodes_from([(new_node, {"specie": species[new_node], "population": 0})])
+    
+    for i in range(len(inter_matrix)):
+        for j in range(i+1,len(inter_matrix)):
+            if inter_matrix[i][j] > 0:
+                #print(inter_matrix[i][j])
+                G.add_edge(i, j, weight=inter_matrix[i][j])
+            
+    
+    pos = nx.spring_layout(G)
+    names = nx.get_node_attributes(G, 'specie')
+    nx.draw(G, pos, node_size=500, with_labels=True)
+    #nx.draw_networkx_labels(G, pos, labels=names)
+    plt.show()
+    
+    return G
+
+
 #################### Main ####################
 if __name__ == "__main__":
     inter_matrix = create_iteraction_matrix()
+    
+    G = create_network(inter_matrix)
+    
+    
