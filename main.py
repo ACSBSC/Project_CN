528b9eb3f37b22755441bffc80ddac3dc3a4280f
ACSBSC <36998939+ACSBSC@users.noreply.github.com>
2022-06-09 13:16:42 +0200

    initial population set

--- main.py
+++ main.py
@@ -45,17 +45,30 @@ def create_iteraction_matrix(filepath=CSV_FILEPATH, self_limitation=SELF_LIMITAT
     return inter_matrix
 
 
-def create_network(inter_matrix, filepath=CSV_FILEPATH_2):
+def create_network(inter_matrix, filepath0=CSV_FILEPATH,filepath=CSV_FILEPATH_2):
     df = pd.read_csv(filepath)  
     species=[]
     for specie in df["Specie"]:
         if specie not in species:
             species.append(specie)
     
+    counter_prey = []
+    df = pd.read_csv(filepath0, header=None)
+    
+    for i, row in df.iterrows():
+        count=0
+        for v in row:
+            if v >0:
+                count+=1
+        counter_prey.append(count)
+    
+    #Initial population is the multiplication of how many predetors the specie has with 
+    #the min number of population of a pray plus their min designated population
+    
     G = nx.Graph()
     
     for new_node in range(len(species)):
-        G.add_nodes_from([(new_node, {"specie": species[new_node], "population": 0})])
+        G.add_nodes_from([(new_node, {"specie": species[new_node], "population": (counter_prey[new_node]*20)+20})])
     
     for i in range(len(inter_matrix)):
         for j in range(i+1,len(inter_matrix)):
@@ -63,6 +76,8 @@ def create_network(inter_matrix, filepath=CSV_FILEPATH_2):
                 #print(inter_matrix[i][j])
                 G.add_edge(i, j, weight=inter_matrix[i][j])
             
+    for node in G.nodes.data():
+        print(node)
     
     pos = nx.spring_layout(G)
     names = nx.get_node_attributes(G, 'specie')
